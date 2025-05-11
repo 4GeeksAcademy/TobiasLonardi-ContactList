9ac2400e59e1364b627b5dd50dac5f30f9722e04
TobiLonardi <123268672+TobiLonardi@users.noreply.github.com>
2025-05-11 18:35:00 +0000

    layout for delete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,11 +48,11 @@ export const Home = () => {
 
 					<div className=" bg-primary-subtle border border-primary-subtle rounded-3">
 						<div className="d-flex justify-content-start w-100">
-							
+
 							<img src="https://i.pravatar.cc/300" alt="" className="profilePicture rounded-circle" />
-							
+
 							<div className="d-flex flex-column align-items-baseline p-4">
-								
+
 								<span className="">
 									<h5>{item.name}</h5>
 
@@ -72,23 +72,41 @@ export const Home = () => {
 									<p>{item.address}</p>
 
 								</span>
-								
-								
+
+
 							</div>
 							<div className="d-flex justify-content-end w-100 ">
 								<i className="fa-solid fa-pencil p-4"></i>
 
-								<i className="fa-solid fa-trash p-4"></i>
+								<i className="fa-solid fa-trash p-4 btn"data-bs-target="#exampleModalToggle" data-bs-toggle="modal"></i>
 							</div>
 						</div>
-													
 
-						
+
+
 
 					</div>
 
 				)}
 			</div>
+			<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+				<div class="modal-dialog modal-dialog-centered">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							are you sure do you want to delete it?
+						</div>
+						<div class="modal-footer">
+							<button class="btn btn-secondary" data-bs-dismiss="modal">delete</button>
+							<button class="btn btn-primary" data-bs-dismiss="modal">cancel</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 		</div>
 	);
 }; 
